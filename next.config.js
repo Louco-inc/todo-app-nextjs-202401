97482d48f3654829327db796e2037dbc319ede6d
mura-work <murata.naoya.work@gmail.com>
2024-01-08 14:35:47 +0900

    corsの修正 一旦全てのオリジンを許可

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
             key: "Access-Control-Allow-Origin",
             // すべてのオリジンを許可するなら * (アスタリスク)
             // ただセキュリティ的にはよろしくないので注意
-            value: "https://todo-app-nextjs-202401-tbt8qcn1r-murata-naoya.vercel.app",
+            value: "*",
           },
           {
             // 許可するメソッド
